004a99e13141e6739e19f95cad2e84145571cc2f
lucky-rar007 <lakshrajsc1234@gmail.com>
2026-04-03 09:12:20 +0530

    trivia bot problem solved

---
+++ javascript_problems/trivia_bot_problem.js
@@ -0,0 +1,14 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="AK";
+let botLocation="BK";
+let favoriteLanguage="CK";
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+let codingFact=favoriteLanguage+" is slow."
+console.log(codingFact);
+codingFact=favoriteLanguage+" is inefficient.";
+console.log(codingFact);
+codingFact=favoriteLanguage+" has 5 data types.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
