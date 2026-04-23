45601f97b02fa367b3c33bcd3e4d08e8a6fa734c
lucky-rar007 <lakshrajsc1234@gmail.com>
2026-04-23 09:32:01 +0530

    sentence maker problem solved

---
+++ javascript_problems/sentence_maker_problem.js
@@ -0,0 +1,19 @@
+let adjective="happy";
+let noun="dog";
+let verb="jump";
+let place="park";
+let adjective2="angry";
+let noun2="cat";
+
+let firstStory="Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: "+firstStory);
+
+adjective="happy";
+noun="cat";
+verb="jump";
+place="park";
+adjective2="angry";
+noun2="dog";
+
+let secondStory="Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second story: "+secondStory);
