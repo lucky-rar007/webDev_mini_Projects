7db18e887013e09603f914492147fa89cf75920e
lucky-rar007 <lakshrajsc1234@gmail.com>
2026-06-25 13:32:01 +0530

    array_deep_flattening_problem solved

---
+++ javascript_problems/array_deep_flattening_problem.js
@@ -0,0 +1,17 @@
+function steamrollArray(arr) {
+  let result = [];
+
+  function flatten(item) {
+    if (Array.isArray(item)) {
+      for (let element of item) {
+        flatten(element);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+
+  flatten(arr);
+
+  return result;
+}
